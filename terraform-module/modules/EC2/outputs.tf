c5ea1e4e8583db8726b04a9f2bee1c214af6fcdc
Abhishek <kuldeepdevda134@gmail.com>
2026-02-19 15:39:24 +0530

    this is latest code

--- terraform-module/modules/EC2/outputs.tf
+++ terraform-module/modules/EC2/outputs.tf
@@ -2,3 +2,6 @@ output "public_ip" {
   value = aws_instance.react_server.public_ip
 }
 
+output "instance_id" {
+  value = aws_instance.react_server.id
+}
---
+++ terraform-module/modules/EC2/variables.tf
@@ -0,0 +1,5 @@
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "instance_name" {}
+variable "sg_name" {}
