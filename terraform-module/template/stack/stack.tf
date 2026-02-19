594e4fce80a83af28a482be4d98e3d9401fa3d15
kuldeepdevda70 <kuldeepdevda134@gmail.com>
2026-02-19 17:33:52 +0530

    Update stack.tf

--- terraform-module/template/stack/stack.tf
+++ terraform-module/template/stack/stack.tf
@@ -8,4 +8,12 @@ module "ec2_server" {
   sg_name       = "react-sg"
 }
 
+# Outputs
+output "ec2_public_ip" {
+  value = module.ec2_server.public_ip
+}
+
+output "ec2_instance_id" {
+  value = module.ec2_server.instance_id
+}
 
